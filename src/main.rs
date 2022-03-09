90d53c14ba3bd16f8a32f1bad8b3601599fe9490
den-taku <dentaku1412@gmail.com>
2022-03-10 05:04:57 +0900

    Copy example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+//! from [here](https://github.com/Pragmatic-Elixir-Meetup/rpi-video-rs/blob/master/examples/simple.rs)
+use rpi_video_rs::recorder::Recorder;
+
 fn main() {
-    println!("Hello, world!");
+    println!("\nStart to record a new H264 video\n");
+
+    let mut recorder = Recperster::new(None);
+
+    match recorder.run() {
+        Ok(res) => println!(
+            "A new H264 video is generated to `{}`\n",
+            res.output_file_path
+        ),
+        Err(err) => println!("An error occurred - `{}`\n", err.message),
+    }
+
+    println!("\nFinish recording\n");
 }
